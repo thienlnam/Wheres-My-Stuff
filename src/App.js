e537b3b29e8396f0ccc9ed371195e1e85e616ae8
Jack Nam <jack.lt.nam@gmail.com>
2020-10-31 14:46:51 -0700

    add starter code

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import 'antd/dist/antd.css';
+import ItemListPage from './pages/ItemListPage';
+import ContainerPage from './pages/ContainerPage';
+import DashboardPage from './pages/DashboardPage';
+import Navbar from './components/Navbar';
+import * as Constants from './constants';
+import { Layout } from 'antd';
+
+
+function App() {
+  const { Header, Content, Footer } = Layout;
+
+  const [currentPage, setPage] = useState(Constants.HEADER_DASHBOARD);
+
+  let pageShown;
+  if (currentPage === Constants.HEADER_ITEMLIST) {
+    pageShown = <ItemListPage />;
+  } else if (currentPage === Constants.HEADER_CONTAINER) {
+    pageShown = <ContainerPage />;
+  } else if (currentPage === Constants.HEADER_DASHBOARD) {
+    pageShown = <DashboardPage />;
+  }
+
+  return (
+    <Layout className="layout">
+      <Header>
+        <Navbar currentPage={currentPage} changePage={setPage} />
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <div className="site-layout-content">
+          {pageShown}
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Capstone CS Team 24 ©2020</Footer>
+    </Layout>
+  );
+}
+
+export default App;
---
+++ src/sampleData.js
@@ -0,0 +1,78 @@
+
+export const sampleItemData = [
+  {
+    name: 'Sanded Plywood 5"',
+    category: 'Wood',
+    container: 'container1',
+    location: 'A1',
+    quantity: 6,
+  },
+  {
+    name: 'Sanded Plywood 10"',
+    category: 'Wood',
+    container: 'container1',
+    location: 'A2',
+    quantity: 12,
+  },
+  {
+    name: 'Hardwood Plywood 5"',
+    category: 'Wood',
+    container: 'container2',
+    location: 'A1',
+    quantity: 5,
+  },
+  {
+    name: 'Hardwood Plywood 10"',
+    category: 'Wood',
+    container: 'container2',
+    location: 'A2',
+    quantity: 10,
+  },
+  {
+    name: 'Plywood Sheathing 5"',
+    category: 'Wood',
+    container: 'container3',
+    location: 'A1',
+    quantity: 4,
+  },
+  {
+    name: 'Plywood Sheathing 10"',
+    category: 'Wood',
+    container: 'container3',
+    location: 'A1',
+    quantity: 8,
+  },
+];
+
+export const sampleContainerData = [
+  {
+    name: 'Garage Container 1',
+    description: 'Contains screws and other small ',
+    identifier: 'Large scratch on the side',
+    location: 'Garage',
+  },
+  {
+    name: 'Red Storage Box',
+    description: 'Holds all the tools',
+    identifier: 'Large red box in the right corner',
+    location: 'Garage',
+  },
+  {
+    name: 'Counter Cabinet',
+    description: 'Contains necklaces',
+    identifier: 'Small white box with 3 drawers',
+    location: 'Hallway',
+  },
+  {
+    name: 'Hank',
+    description: 'Holds large machinery units',
+    identifier: 'Larged wheeled black cabinet',
+    location: 'Garage',
+  },
+  {
+    name: 'Bilbo',
+    description: 'Contains large scratch on the side',
+    identifier: 'Red cabinet with 6 drawers',
+    location: 'Bedroom',
+  },
+];
