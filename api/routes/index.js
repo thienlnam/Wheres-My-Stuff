461f4a0c6c80196c1042c12a5e56dfb04439e318
friesen04 <40907762+friesen04@users.noreply.github.com>
2020-11-21 16:13:08 -0800

    linting errors

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,63 +1,63 @@
-var express = require('express');
-var router = express.Router();
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'master',
-    password: 'Wh3r3$MyStuff',
-    database: 'WMSInventory'
+const express = require('express')
+const router = express.Router()
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'master',
+  password: 'Wh3r3$MyStuff',
+  database: 'WMSInventory'
 })
 
 
 function filterParts(table, criteria, filter, context, callback) {
-    var sql = "SELECT * FROM wmsinventory." + table + " WHERE " + criteria + "=" + filter;
-    connection.query(sql, function (err, rows) {
-        if (err) {
-            throw err;
-        }
-        context.filter = rows;
-        return callback();
-    });
+  var sql = 'SELECT * FROM wmsinventory.' + table + ' WHERE ' + criteria + '=' + filter
+  connection.query(sql, function (err, rows) {
+    if (err) {
+      throw err
+    }
+    context.filter = rows
+    return callback()
+  })
 }
 
-router.get("/filterParts", function (req, res) {
-    console.log("Filtering parts");
-    var context = {};
-    var criteria = "name";
-    var filter = "\"Jackhammer\""
-    filterParts("Parts", criteria, filter, context, callback);
-    function callback() {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).send(JSON.stringify(context.filter));
-    }
+router.get('/filterParts', function (req, res) {
+  console.log('Filtering parts')
+  var context = {}
+  var criteria = 'name'
+  var filter = '\"Jackhammer\"'
+  filterParts('Parts', criteria, filter, context, callback)
+  function callback() {
+    res.setHeader('Content-Type', 'application/json')
+    res.status(200).send(JSON.stringify(context.filter))
+  }
 })
 
 
 function getParts(table, context, callback) {
-    var sql = "SELECT * FROM wmsinventory." + table;
-    connection.query(sql, function (err, rows) {
-        if (err) {
-            throw err;
-        }
-        context.parts = rows
-        callback();
-    });
+  var sql = 'SELECT * FROM wmsinventory.' + table
+  connection.query(sql, function (err, rows) {
+    if (err) {
+      throw err
+    }
+    context.parts = rows
+    callback()
+  })
 }
 
-router.get("/getParts", function (req, res) {
-    console.log("Getting all parts");
-    var context = {};
-    getParts("Parts", context, callback);
-    function callback() {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).send(JSON.stringify(context.parts));
-    }
+router.get('/getParts', function (req, res) {
+  console.log('Getting all parts')
+  var context = {}
+  getParts('Parts', context, callback)
+  function callback() {
+    res.setHeader('Content-Type', 'application/json')
+    res.status(200).send(JSON.stringify(context.parts))
+  }
 })
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  res.render('index', { title: 'Express' })
+})
 
-module.exports = router;
+module.exports = router
--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,100 +1,100 @@
-var express = require('express');
-var router = express.Router();
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'master',
-    password: 'Wh3r3$MyStuff',
-    database: 'WMSInventory'
+const express = require('express')
+const router = express.Router()
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'master',
+  password: 'Wh3r3$MyStuff',
+  database: 'WMSInventory'
 })
 
 // ***PARTS Queries*** //
 function filterParts(table, criteria, filter, context, callback) {
-    var sql = "SELECT * FROM wmsinventory." + table + " WHERE " + criteria + "=" + filter;
-    connection.query(sql, function (err, rows) {
-        if (err) {
-            throw err;
-        }
-        context.filter = rows;
-        return callback();
-    });
+  var sql = 'SELECT * FROM wmsinventory.' + table + ' WHERE ' + criteria + '=' + filter
+  connection.query(sql, function (err, rows) {
+    if (err) {
+      throw err
+    }
+    context.filter = rows
+    return callback()
+  })
 }
 
-router.get("/filterParts", function (req, res) {
-    console.log("Filtering parts");
-    var context = {};
-    filterParts("Parts", "name", "\"Jackhammer\"", context, callback);
-    function callback() {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).send(JSON.stringify(context.filter));
-    }
+router.get('/filterParts', function (req, res) {
+  console.log('Filtering parts')
+  var context = {}
+  filterParts('Parts', 'name', '\"Jackhammer\"', context, callback)
+  function callback() {
+    res.setHeader('Content-Type', 'application/json')
+    res.status(200).send(JSON.stringify(context.filter))
+  }
 })
 
 
 function getParts(table, context, callback) {
-    var sql = "SELECT * FROM wmsinventory." + table;
-    connection.query(sql, function (err, rows) {
-        if (err) {
-            throw err;
-        }
-        context.parts = rows
-        callback();
-    });
+  var sql = 'SELECT * FROM wmsinventory.' + table
+  connection.query(sql, function (err, rows) {
+    if (err) {
+      throw err
+    }
+    context.parts = rows
+    callback()
+  })
 }
 
-router.get("/getParts", function (req, res) {
-    console.log("Getting all parts");
-    var context = {};
-    getParts("Parts", context, callback);
-    function callback() {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).send(JSON.stringify(context.parts));
-    }
+router.get('/getParts', function (req, res) {
+  console.log('Getting all parts')
+  var context = {}
+  getParts('Parts', context, callback)
+  function callback() {
+    res.setHeader('Content-Type', 'application/json')
+    res.status(200).send(JSON.stringify(context.parts))
+  }
 })
 
 
 function createPart( name, category, quantity, location, callback) {
-    var sql = "INSERT INTO wmsinventory.Parts (name, category, partQuantity, partLocation) VALUES (" + name + ", " + category + ", " + quantity + ", " + location + ")";
-    connection.query(sql, function (err) {
-        if (err) {
-            throw err;
-        }
-        return callback();
-    });
+  var sql = 'INSERT INTO wmsinventory.Parts (name, category, partQuantity, partLocation) VALUES (' + name + ', ' + category + ', ' + quantity + ', ' + location + ')'
+  connection.query(sql, function (err) {
+    if (err) {
+      throw err
+    }
+    return callback()
+  })
 }
 
-createPart("\"Jackhammer\"", "\"hammer\"", "2", "\"garage\"", function () {
-    router.route('/createTestPart')
-        .post(function (req, res, next) {
-        console.log("Created Part\n");
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).send(JSON.stringify("Part was successfully added"));
-    });
-});
+createPart('\"Jackhammer\"', '\"hammer\"', '2', '\"garage\"', function () {
+  router.route('/createTestPart')
+  .post(function (req, res, next) {
+  console.log('Created Part\n')
+  res.setHeader('Content-Type', 'application/json')
+  res.status(200).send(JSON.stringify('Part was successfully added'))
+  })
+})
 
 
 function deletePart(criteria, filter, callback) {
-    var sql = "DELETE FROM WMSInventory.Parts WHERE " + criteria + " = " + filter;
-    connection.query(sql, function (err) {
-        if (err) {
-            throw err;
-        }
-        return callback();
-    });
+  var sql = 'DELETE FROM WMSInventory.Parts WHERE ' + criteria + ' = ' + filter
+  connection.query(sql, function (err) {
+    if (err) {
+      throw err
+    }
+    return callback()
+  })
 }
 
-deletePart("name", "\"ScrewA\"", function () {
-    router.route('/deleteTestPart')
-        .delete(function (req, res, next) {
-        console.log("Deleted Part\n");
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).send(JSON.stringify("Part was successfully deleted"));
-    });
-});
+deletePart('name', '\"ScrewA\"', function () {
+  router.route('/deleteTestPart')
+  .delete(function (req, res, next) {
+  console.log('Deleted Part\n')
+  res.setHeader('Content-Type', 'application/json')
+  res.status(200).send(JSON.stringify('Part was successfully deleted'))
+  })
+})
 
 
 router.route('/')
-    .get(function (req, res, next) {
-    res.send('API is working properly');
-});
+  .get(function (req, res, next) {
+  res.send('API is working properly')
+})
 module.exports = router;
