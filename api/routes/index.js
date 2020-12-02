0eb90a8f55d4bbab9522bff598c19a67ddea0034
Jack Nam <jack.lt.nam@gmail.com>
2020-12-02 12:11:55 -0800

    Make API more RESTFUL

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,25 +1,29 @@
+require('dotenv').config({ path: '../.env' })
+
 const express = require('express')
 const router = express.Router()
 const mysql = require('mysql')
+
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
+  port: process.env.DB_PORT || 3306,
   database: 'WMSInventory'
 })
 
 // ***Queries*** //
-function getItems (req, context, callback) {
+function getItems (req, callback) {
   let sql = mysql.format('SELECT * FROM wmsinventory.?', [
     req.body.table
   ])
   sql = sql.replace(/['"]/g, '')
-  connection.query(sql, function (err, rows) {
+  connection.query(sql, function (err, result) {
     if (err) {
-      throw err
+      callback(err, null);
+    } else {
+      callback(null, result);
     }
-    context.parts = rows
-    callback()
   })
 }
 
@@ -32,16 +36,18 @@ function filterItems (req, context, callback) {
   for (let i = 0; i < 4; i++) {
     sql = sql.replace(/["']/, '')
   }
-  connection.query(sql, function (err, rows) {
+  connection.query(sql, function (err, result) {
     if (err) {
-      throw err
+      callback(err, null);
+    } else {
+      callback(null, result);
     }
-    context.filter = rows
-    callback()
   })
 }
 
 function deleteItem (req, callback) {
+  // TODO: Add check if item exists
+  // TODO: Delete if exists otherwise give error code and message
   let sql = mysql.format('DELETE FROM WMSInventory.? WHERE ? = ?', [
     req.body.table,
     req.body.criteria,
@@ -50,11 +56,12 @@ function deleteItem (req, callback) {
   for (let i = 0; i < 4; i++) {
     sql = sql.replace(/["']/, '')
   }
-  connection.query(sql, function (err) {
+  connection.query(sql, function (err, result) {
     if (err) {
-      throw err
+      callback(err, null);
+    } else {
+      callback(null, result);
     }
-    callback()
   })
 }
 
@@ -66,11 +73,12 @@ function createPart (req, callback) {
     req.body.quantity,
     req.body.location
   ])
-  connection.query(sql, function (err) {
+  connection.query(sql, function (err, result) {
     if (err) {
-      throw err
+      callback(err, null);
+    } else {
+      callback(null, result);
     }
-    callback()
   })
 }
 
@@ -82,24 +90,27 @@ function createContainer (req, callback) {
     req.body.location,
     req.body.description
   ])
-  connection.query(sql, function (err) {
+  connection.query(sql, function (err, result) {
     if (err) {
-      throw err
+      callback(err, null);
+    } else {
+      callback(null, result);
     }
-    callback()
   })
 }
 
 function createUser (req, callback) {
+  // TODO: Either encrypt password OR use a different oauth provider
   const sql = mysql.format('INSERT INTO wmsinventory.Users (username, password) VALUES (?, ?)', [
     req.body.username,
     req.body.password
   ])
-  connection.query(sql, function (err) {
+  connection.query(sql, function (err, result) {
     if (err) {
-      throw err
+      callback(err, null);
+    } else {
+      callback(null, result);
     }
-    callback()
   })
 }
 
@@ -107,22 +118,28 @@ function createCategory (req, callback) {
   const sql = mysql.format('INSERT INTO wmsinventory.Categories (name) VALUES (?)', [
     req.body.name
   ])
-  connection.query(sql, function (err) {
+  connection.query(sql, function (err, result) {
     if (err) {
-      throw err
+      callback(err, null);
+    } else {
+      callback(null, result);
     }
-    callback()
   })
 }
 
 // ***Test Routes*** //
 router.get('/getItems', function (req, res) {
-  console.log('Getting all ' + req.body.tables)
-  const context = {}
-  getItems(req, context, callback)
-  function callback () {
-    res.setHeader('Content-Type', 'application/json')
-    res.status(200).send(JSON.stringify(context.parts))
+  console.log('Getting all ' + req.body.table)
+  getItems(req, callback)
+  function callback(err, data) {
+    if (err) {
+      console.log(err);
+      res.setHeader('Content-Type', 'application/json');
+      res.status(err.status || 400).json({status: err.status, message: err.message});
+    } else {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(data);
+    }
   }
 })
 
@@ -130,65 +147,110 @@ router.get('/filterItems', function (req, res) {
   console.log('Filtering ' + req.body.table)
   const context = {}
   filterItems(req, context, callback)
-  function callback () {
-    res.setHeader('Content-Type', 'application/json')
-    res.status(200).send(JSON.stringify(context.filter))
+  function callback(err, data) {
+    if (err) {
+      console.log(err);
+      res.setHeader('Content-Type', 'application/json');
+      res.status(err.status || 400).json({status: err.status, message: err.message});
+    } else {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(data);
+    }
   }
 })
 
 router.delete('/deleteItem', function (req, res) {
   console.log('Deleting ' + req.body.table)
   deleteItem(req, callback)
-  function callback () {
-    console.log('Deleted ' + req.body.filter)
-    res.setHeader('Content-Type', 'application/json')
-    res.status(200).send(JSON.stringify(req.body.filter + ' was successfully deleted'))
+  function callback (err) {
+    if (err) {
+      console.log(err);
+      res.setHeader('Content-Type', 'application/json');
+      res.status(err.status || 400).json({status: err.status, message: err.message});
+    } else {
+      res.setHeader('Content-Type', 'application/json')
+      res.status(204).json();
+    }
   }
 })
 
 router.post('/createPart', function (req, res) {
   console.log('Creating Part')
   createPart(req, callback)
-  function callback () {
-    console.log('Created ' + req.body.name)
-    res.setHeader('Content-Type', 'application/json')
-    res.status(200).send(JSON.stringify(req.body.name + ' was successfully added'))
+  function callback (err, data) {
+    if (err) {
+      console.log(err);
+      res.setHeader('Content-Type', 'application/json');
+      res.status(err.status || 400).json({status: err.status, message: err.message});
+    } else {
+      res.setHeader('Content-Type', 'application/json')
+      res.status(201).json({
+        id: data.insertId,
+        name: req.body.name,
+        category: req.body.category,
+        quantity: req.body.quantity,
+        locaton: req.body.location
+      });
+    }
   }
 })
 
 router.post('/createContainer', function (req, res) {
   console.log('Creating Container')
   createContainer(req, callback)
-  function callback () {
-    console.log('Created ' + req.body.name)
-    res.setHeader('Content-Type', 'application/json')
-    res.status(200).send(JSON.stringify(req.body.name + ' was successfully added'))
+  function callback (err, data) {
+    if (err) {
+      console.log(err);
+      res.setHeader('Content-Type', 'application/json');
+      res.status(err.status || 400).json({status: err.status, message: err.message});
+    } else {
+      res.setHeader('Content-Type', 'application/json')
+      res.status(201).json({
+        id: data.insertId,
+        name: req.body.name,
+        quantity: req.body.quantity,
+        size: req.body.size,
+        location: req.body.location,
+        description: req.body.description
+      });
+    }
   }
 })
 
 router.post('/createUser', function (req, res) {
   console.log('Creating User')
   createUser(req, callback)
-  function callback () {
-    console.log('Created User')
-    res.setHeader('Content-Type', 'application/json')
-    res.status(200).send(JSON.stringify('User was successfully added'))
+  function callback (err, data) {
+    if (err) {
+      console.log(err);
+      res.setHeader('Content-Type', 'application/json');
+      res.status(err.status || 400).json({status: err.status, message: err.message});
+    } else {
+      res.setHeader('Content-Type', 'application/json')
+      res.status(201).json({
+        id: data.insertId,
+        username: req.body.username,
+      });
+    }
   }
 })
 
 router.post('/createCategory', function (req, res) {
   console.log('Creating Category')
   createCategory(req, callback)
-  function callback () {
-    console.log('Created ' + req.body.name)
-    res.setHeader('Content-Type', 'application/json')
-    res.status(200).send(JSON.stringify(req.body.name + ' was successfully added'))
+  function callback (err, data) {
+    if (err) {
+      console.log(err);
+      res.setHeader('Content-Type', 'application/json');
+      res.status(err.status || 400).json({status: err.status, message: err.message});
+    } else {
+      res.setHeader('Content-Type', 'application/json')
+      res.status(201).json({
+        id: data.insertId,
+        name: req.body.name
+      });
+    }
   }
-})
-
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
 })
 
 module.exports = router
